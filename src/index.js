48853fb51b43a254d93e60ccb00554e36b8f55ee
Gina <floresgina2020@gmail.com>
2021-06-16 22:33:46 -0500

    ya casi esta listo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-// import validator from './validator.js'; //
+import validator from './validator.js';
 
-// Validar solo números sin letras y caractéres//
+// Validar solo numeros sin letras y caracteres//
 const validarNumeros = document.getElementById("CardNumber");
-
-    validarNumeros.addEventListener('keypress', (event) => {
+validarNumeros.addEventListener('keypress', (event) => {
     event.preventDefault();
     let codigoKey = event.keyCode
     let valorKey = String.fromCharCode(codigoKey)
-    //console.log(valorKey)//
+    //console.log(valorKey)
 
     let valor =  parseInt(valorKey)
-    //console.log(valor)//
+    //console.log(valor)validator.isValid(datoNumero)
 
     if (valor || valor === 0) {
         validarNumeros.value += valor
@@ -18,4 +17,49 @@ const validarNumeros = document.getElementById("CardNumber");
 })
 
 //Obtener los datos del input//
+const validatorButton = document.getElementById("btn");
+validatorButton.addEventListener("click", () => {
+    // Obtener dato del nombre
+    const userName = document.getElementById("UserName").value;
+    //Obtener el dato del numero de tarjeta
+    const cardNumber = document.getElementById("CardNumber");
+    let datoNumero = cardNumber.value;
+    validator.isValid(datoNumero);
+
+    // Declarar para mostrar ultimos 4 digitos
+    const mostrarUltimo = document.getElementById("CardNumber")
+    let ultimoDigito = mostrarUltimo.value;
+    validator.maskify(ultimoDigito)
+
+    if ((datoNumero.length < 12 || datoNumero.length > 19) && datoNumero.length != 0) {
+        alert("El número de tarjeta tiene que ser mayor a 12 y menor a 19 dígitos");
+        document.getElementById('CardNumber').value = "";
+        document.getElementById('UserName').value = "";
+    }
+    if ((datoNumero == "") || (userName == "")) {
+        alert("Los campos no deben de estar vacios");
+        document.getElementById('CardNumber').value = "";
+        document.getElementById('UserName').value = "";
+        return false;
+    }
+    // Ocultar formulario
+    const ocultar = document.getElementById("btn");
+    ocultar.addEventListener("click", () => {
+        document.getElementById('hide-form').style.display = 'none';
+    })
+    // Mostar si la tarjeta es valida e invalida
+    const botonValidar = document.getElementById("btn");
+    botonValidar.addEventListener("click", () => {
+        if (validator.isValid(datoNumero)){
+            document.getElementById('show-valid').innerText = "La tarjeta " + validator.maskify(ultimoDigito) + " es válida." + " Puedes pagar o hacer transferencia.";
+            document.getElementById('show-valid').style.display = 'block';
+            document.getElementById('footer').style.display = 'none';
+        } else {
+            document.getElementById('show-invalid').innerText = "La tarjeta es inválida."+ " Ingrese número válido.";
+            document.getElementById('show-invalid').style.display = 'block';
+            document.getElementById('footer').style.display = 'none';
+            document.getElementById('')
+        }
+    })
+})
 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,42 @@
 const validator = {
-  // ...
+    isValid: function(creditCardNumber) {
+        //Algoritmo de luhn
+        const convertado = creditCardNumber.split('').reverse();
+        for (let i = 0; i < convertado.length; i++) {
+            convertado[i] = parseInt (convertado [i], 10);
+        }
+        let sum = 0;
+        let esPar = convertado.length % 2 === 0;
+        for(let i = convertado.length - 1; i >= 0; i--) {
+            if (esPar) {
+                convertado [i] *= 2;
+                if (convertado [i] >= 10) {
+                    convertado [i] -= 9;
+                }
+            }
+            sum += convertado [i];
+            esPar =! esPar;
+        }
+        if (sum % 10 === 0) {
+            return true
+            //console.log("valido");
+        } else {
+            return false
+            //console.log("invalido");
+        }
+    },
+    maskify: function(convertado) {
+        let countNum = '';
+
+        for(let i = 0; i< convertado.length; i++) {
+            if (i <= convertado.length - 5) {
+                countNum = countNum + '#'
+            } else {
+                countNum = countNum + convertado[i];
+            }
+        }
+        return countNum;
+    }
 };
 
 export default validator;
